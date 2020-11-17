951fd6aa4717f1a16596c0a58133b0e57e358c86
Jacob Ruleaux <jakeruleaux@gmail.com>
2020-11-16 19:41:07 -0700

    import and convert feather svg to use on splash screen

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
+import React from 'react';
 import './App.css';
+import Feather from './images/feather.js';
 
 function App() {
   return (
     <div className="App">
       App
+      <Feather className="feather" />
     </div>
   );
 }
