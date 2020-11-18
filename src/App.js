591a2200bd34599aaa1c86800dff1b210ec6a56a
Jacob Ruleaux <jakeruleaux@gmail.com>
2020-11-17 20:04:11 -0700

    add product list comp and product directory with test images

--- src/App.js
+++ src/App.js
@@ -1,14 +1,13 @@
 import React from 'react';
 import './App.css';
 import Splash from './comps/Splash.js';
-// import Feather from './images/feather.js';
+import ProductList from './comps/ProductList';
 
 function App() {
   return (
     <div className="App">
       <Splash />
-      {/* <h1 className="main-header">feather</h1>
-      <Feather  /> */}
+      <ProductList />
     </div>
   );
 }
---
+++ src/comps/ProductList.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import styled, { keyframes } from 'styled-components';
+
+const ProductListContainer = styled.div `
+
+`;
+
+const ProductList = () => {
+    return (
+        <ProductListContainer>P-list</ProductListContainer>
+    );
+}
+
+export default ProductList;
